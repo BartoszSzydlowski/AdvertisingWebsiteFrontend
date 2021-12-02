47e65e25261cdcb9977acb61c2f83bb96346d4b7
Bartosz Szydłowski <bbartek311@gmail.com>
2021-12-02 15:44:10 +0100

    fixed missing navbar

---
+++ src/components/navbar/navbar.tsx
@@ -0,0 +1,46 @@
+import { Nav, NavLogo, NavLink, Bars, NavMenu, } from "./navbarElements";
+
+const Navbar = (props: any) => {
+    return (
+        <>
+            <Nav>
+                <NavLogo to="/">
+                    Logo
+                </NavLogo>
+
+                <Bars/>
+
+                <NavMenu>
+                    <NavLink to="/home">
+                        Home
+                    </NavLink>
+
+                    <NavLink to="/about">
+                        About
+                    </NavLink>
+
+                    <NavLink to="/createAdvert">
+                        Add new advert
+                    </NavLink>
+
+                    {!props.isLoggedIn ? (
+                        <>
+                            <NavLink to="/login">
+                                Login
+                            </NavLink>
+
+                            <NavLink to="/register">
+                                Register
+                            </NavLink>
+                        </>
+                        ) : (
+                        <NavLink to="/logout" onClick={props.logout()}>
+                            Logout
+                        </NavLink>
+                    )}
+                </NavMenu>
+            </Nav>
+        </>
+    );
+};
+export default Navbar;
