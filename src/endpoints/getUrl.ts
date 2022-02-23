8f938b7ddad6f5ebe02e85e8bf2d37ba6333a7b1
Bartosz Szydłowski <bbartek311@gmail.com>
2022-02-23 13:30:18 +0100

    removed eslint warning

--- src/endpoints/getUrl.ts
+++ src/endpoints/getUrl.ts
@@ -1,7 +1,10 @@
 const getUrl = (): string => {
   let url = '';
   console.log(process.env);
-  if (!process.env.REACT_APP_ENV || process.env.REACT_APP_ENV === 'development') {
+  if (
+    !process.env.REACT_APP_ENV ||
+    process.env.REACT_APP_ENV === 'development'
+  ) {
     url = 'https://localhost:44320';
   } else {
     url = 'XDDDDDDDDDDDDD';
