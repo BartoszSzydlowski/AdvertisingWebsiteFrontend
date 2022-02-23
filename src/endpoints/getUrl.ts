281bcebf3a36af1dc7d1b3789966c591a688f110
Bartosz Szydłowski <bbartek311@gmail.com>
2022-02-23 13:47:06 +0100

    changed url dev check

--- src/endpoints/getUrl.ts
+++ src/endpoints/getUrl.ts
@@ -1,13 +1,11 @@
 const getUrl = (): string => {
   let url = '';
   console.log(process.env);
-  if (
-    !process.env.REACT_APP_ENV ||
-    process.env.REACT_APP_ENV === 'development'
-  ) {
+  if (process.env.REACT_APP_ENV === 'production') {
+    url = 'https://XDDDDDDDDDDDDD.com';
+  }
+  if (process.env.REACT_APP_ENV === 'development') {
     url = 'https://localhost:44320';
-  } else {
-    url = 'XDDDDDDDDDDDDD';
   }
   return url;
 };
