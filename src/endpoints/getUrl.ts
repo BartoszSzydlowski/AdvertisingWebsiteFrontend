028c38f28acb034571534ac61092e00642fe04b2
Bartosz Szydłowski <bbartek311@gmail.com>
2022-02-23 17:58:27 +0100

    change prod url

--- src/endpoints/getUrl.ts
+++ src/endpoints/getUrl.ts
@@ -1,8 +1,7 @@
 const getUrl = (): string => {
   let url = '';
-  console.log(process.env);
   if (process.env.REACT_APP_ENV === 'production') {
-    url = 'https://advwebsitebszydlowski.gateway.api.zanreal.pl';
+    url = 'https://advwebsitebszydlowski.zanreal.pl';
   }
   if (process.env.REACT_APP_ENV === 'development') {
     url = 'https://localhost:44320';
