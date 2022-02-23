cc54aa1b0e30a3b1367b5675eb76cf44d01c0733
Bartosz Szydłowski <bbartek311@gmail.com>
2022-02-23 03:00:01 +0100

    added console log

--- src/endpoints/getUrl.ts
+++ src/endpoints/getUrl.ts
@@ -4,9 +4,9 @@ const getUrl = (): string => {
   let url = '';
   if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
     url = 'https://localhost:44320';
-    console.log("DEV MODE")
+    console.log('DEV MODE');
   } else {
-    console.log("PROD MODE")
+    console.log('PROD MODE');
     url = 'XDDDDDDDDDDDDD';
   }
   return url;
