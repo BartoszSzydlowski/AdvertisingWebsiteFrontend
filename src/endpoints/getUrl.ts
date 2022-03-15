af7a0273a9bfa9df6864abfbc634d3cfbda940b7
Bartosz Szydłowski <bbartek311@gmail.com>
2022-03-15 13:16:37 +0100

    changed domain

--- src/endpoints/getUrl.ts
+++ src/endpoints/getUrl.ts
@@ -1,7 +1,8 @@
 const getUrl = (): string => {
   let url = '';
   if (process.env.REACT_APP_ENV === 'production') {
-    url = 'https://advwebsitebszydlowski.zanreal.pl';
+    //url = 'https://advwebsitebszydlowski.zanreal.pl';
+    url = 'https://advertisingwebsitebszydlowski.com.pl';
   }
   if (process.env.REACT_APP_ENV === 'development') {
     url = 'https://localhost:44320';
